9915d0fbbadfcc67bbce1c4ac7f02c641eed3187
sparkology <kunalgupta.rs12223@gmail.com>
2025-11-08 21:35:18 +0530

    Replace old code with new backend code

--- routes/announcements.js
+++ routes/announcements.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 const { createAnnouncement, getAnnouncements, getAnnouncementById, updateAnnouncement, deleteAnnouncement } = require('../controllers/announcementController');
 const { protect } = require('../middleware/auth');
